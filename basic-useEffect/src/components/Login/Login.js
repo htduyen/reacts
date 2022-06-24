cc0ab5576879aa79e6fb55c068f713f8869d6a7f
Raymond Trinh <rtrinh@bluecatnetworks.com>
2022-06-24 21:39:53 +0700

    Using custom context

--- basic-useEffect/src/components/Login/Login.js
+++ basic-useEffect/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useEffect, useReducer, useState, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
     if (action.type === "USER_INPUT") {
@@ -24,6 +25,7 @@ const passwordReducer = (state, action) => {
     return { value: "", isValid: false };
 };
 const Login = (props) => {
+    const authCtx = useContext(AuthContext);
     // const [enteredEmail, setEnteredEmail] = useState("");
     // const [emailIsValid, setEmailIsValid] = useState();
     // const [enteredPassword, setEnteredPassword] = useState("");
@@ -88,7 +90,7 @@ const Login = (props) => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(emailState.value, passwordState.value);
+        authCtx.onLogin(emailState.value, passwordState.value);
     };
 
     return (
